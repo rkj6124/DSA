1dddffac49333abb9fe3e2e3cc8c2567a304130e
rkj6124 <rkjproton@gmail.com>
2022-07-15 15:25:44 +0530

    initialising project

---
+++ src/java/main/DPAlgo/CatalanNumber.java
@@ -0,0 +1,14 @@
+package java.main.DPAlgo;
+
+public class CatalanNumber {
+
+    public static void main(String[] args) {
+        int n;
+
+    }
+
+    public static int calculate_catalan(int n) {
+
+        return 0;
+    }
+}
---
+++ src/java/main/DPAlgo/DPAlgo.java
@@ -0,0 +1,4 @@
+package java.main.DPAlgo;
+
+public class DPAlgo {
+}
