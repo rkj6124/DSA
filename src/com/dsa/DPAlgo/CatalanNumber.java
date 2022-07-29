789595a9bb695535d0c980cc921e75a21b45dd46
Ravi Kumar Jha <rkjproton@gmail.com>
2022-07-29 21:45:57 +0530

    Nth Catalan Number
    added method to calculate nth catalan number using formula

--- src/com/dsa/DPAlgo/CatalanNumber.java
+++ src/com/dsa/DPAlgo/CatalanNumber.java
@@ -5,16 +5,22 @@ import java.util.ArrayList;
 public class CatalanNumber {
 
     public static void main(String[] args) {
-        int n = 5;
+        int n = 20;
         System.out.println(calculate_catalan(n));
+        System.out.println(calculate_catalan_with_formula(n));
+    }
+
+    /**
+     * @param n nth number ?
+     *          <p><b>formula:</b> 1/(n+1) * 2nCn => (2n! / n! * n!) / (n + 1)</p>
+     *
+     * @return <strong>nth catalan number</strong>
+     */
+    private static int calculate_catalan_with_formula(int n) {
+        return BinomialCoefficient.calculate_binomial_coefficient(2*n, n)/(n+1);
     }
 
     public static int calculate_catalan(int n) {
-        ArrayList<ArrayList<Integer>> catalan = new ArrayList<>();
-        if (n < 2) return 1;
-        for (int i = 0; i < n; i++) {
-//            catalan.
-        }
         return 0;
     }
 }
